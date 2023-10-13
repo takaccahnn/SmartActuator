44c5ff30897eff4d1b3711e7857510bc535ec778
TakakiHoriuchiLab <u043006j@ecs.osaka-u.ac.jp>
2023-10-13 11:38:43 +0900

    簡単なHEBIの操作を行うプログラムの作成

--- SmartActuatorOperation.py
+++ SmartActuatorOperation.py
@@ -1,99 +1,24 @@
-/*
- * Send effort commands and log in the background.
- *
- * For more information, go to http://docs.hebi.us/tools.html#cpp-api
- *
- * This script assumes you can create a group with 1 module.
- *
- * HEBI Robotics
- * September 2018
-*/
+import hebi 
+# HEBI RoboticsのPython APIをインポートします
+from time import sleep 
+# sleep関数をインポートします
 
-#include <iostream>
-#include <chrono>
-#include <thread>
-#include "lookup.hpp"
-#include "group_command.hpp"
-#include "group_feedback.hpp"
-#include "log_file.hpp"
-#include "util/plot_functions.h"
 
-namespace plt = matplotlibcpp;
+lookup = hebi.Lookup()
+ # ネットワーク上のHEBIモジュールを検索するためのLookupオブジェクトを作成します
+sleep(2)
+ # モジュールがネットワークに登録されるのを待つために2秒間スリープします
 
-using namespace hebi;
+group = lookup.get_group_from_names(["Family"], ["Name"]) 
+# 特定のファミリ名と名前を持つモジュールからなるグループを取得します
 
-int main() {
-  // Get group
-  Lookup lookup;
-  auto group = lookup.getGroupFromNames({"Test Family"}, {"Test Actuator" });
+if group is None: 
+# グループが見つからない場合、エラーメッセージを表示してプログラムを終了します
+  print("モジュールが見つかりません")
+  exit(1)
 
-  if (!group) {
-    std::cout
-      << "Group not found! Check that the family and name of a module on the network" << std::endl
-      << "matches what is given in the source file." << std::endl;
-    return -1;
-  }
+command = hebi.GroupCommand(group.size) 
+# グループのサイズに基づいてGroupCommandオブジェクトを作成します
 
-  //// Open-loop controller (effort)
-
-  // The command struct has fields for various commands and settings; for the
-  // actuator, we will primarily use position, velocity, and effort.
-  //
-  // Fields that are not filled in will be ignored when sending.
-  GroupCommand group_command(group->size());
-  // GroupCommand uses Eigen types for data interchange
-  Eigen::VectorXd efforts(1);
-  // Allocate feedback
-  GroupFeedback group_feedback(group->size());
-  
-  // Start logging in the background
-  std::string log_path = group->startLog("./logs");
-
-  if (log_path.empty()) {
-    std::cout << "~~ERROR~~\n"
-              << "Target directory for log file not found!\n"
-              << "HINT: Remember that the path declared in 'group->startLog()' "
-              << "is relative to your current working directory...\n";
-    return 1;
-  }
-
-  // Parameters for sin/cos function
-  double freq_hz = 0.5;               // [Hz]
-  double freq = freq_hz * 2.0 * M_PI; // [rad / sec]
-  double amp = 1.0;                   // [Nm]
-
-  double duration = 10;               // [sec]
-  auto start = std::chrono::system_clock::now();
-
-  std::chrono::duration<double> t(std::chrono::system_clock::now() - start);
-  while (t.count() < duration) {
-    // Even though we don't use the feedback, getting feedback conveniently
-    // limits the loop rate to the feedback frequency
-    group->getNextFeedback(group_feedback);
-
-    // Update effort set point
-    t = std::chrono::system_clock::now() - start;
-    efforts[0] = amp * std::sin(freq * t.count());
-    group_command.setEffort(efforts);
-    group->sendCommand(group_command);
-  }
-
-  // Stop logging
-  auto log_file = group->stopLog();
-
-  //plot logged effort
-  std::vector<std::vector<double>> eff;
-  eff.resize(group->size());
-  GroupFeedback fbk(group->size());
-  while(log_file->getNextFeedback(fbk)) {
-    for(size_t i = 0; i < group->size(); i++){
-      eff[i].push_back(fbk.getEffort()[i]);
-    }
-  }
-  for(size_t i = 0; i < group->size(); i++){
-    plt::plot(eff[i]);
-  }
-  plt::show();
-
-  return 0;
-}
+command.velocity = [1.0] 
+# 回転数（rad/sec）を設定します。ここでは1.0 rad/secに設定しています
